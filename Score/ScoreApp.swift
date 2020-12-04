2b5ab7001f0f7e4a56a6d4510409a64472014c64
yakudZZZa <yakudZZZa@list.ru>
2020-12-04 15:25:02 +0500

    Initial Commit

---
+++ Score/ScoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScoreApp.swift
+//  Score
+//
+//  Created by Евгений Иванов on 04.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct ScoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
